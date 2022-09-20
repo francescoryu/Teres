af95776c2e80130af49173ac25021bbc9ab33fef
francescoryu <francescoryu04@gmail.com>
2022-09-20 19:54:06 +0200

    did some stuff

--- MainMenuMethods.py
+++ MainMenuMethods.py
@@ -1,7 +1,10 @@
 import time
 
 
+
 def sleep_after_interaction(x):
     time.sleep(x)
 
-
+def get_input():
+    menu_selection_input = input()
+    print("You selected: " + menu_selection_input)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import MainMenuMethods
+import Menu
 
 if __name__ == '__main__':
-    print("Welcome! This is my first terminal-based Python Project")
-    MainMenuMethods.sleep_after_interaction(1)
+    Menu
