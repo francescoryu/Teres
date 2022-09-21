48a9b87de64528fcb15e87b70495e862ba65e34c
Francesco Ryu <francescoryu04@gmail.com>
2022-09-21 13:39:26 +0200

    edited some stuff

--- MainMenuMethods.py
+++ MainMenuMethods.py
@@ -1,10 +1,57 @@
+import webbrowser
+from datetime import datetime
+from random import randint
 import time
 
+__author__ = "Francesco Ryu"
+__date__ = "2022/09/21"
+__Version__ = "0.1"
 
-
-def sleep_after_interaction(x):
+def sleep_after_interaction(x=float):
     time.sleep(x)
 
-def get_input():
-    menu_selection_input = input()
-    print("You selected: " + menu_selection_input)
+
+def start_selected_progress():
+    selection_number = int
+    while selection_number != 4:
+        print("-----------------------------------")
+        sleep_after_interaction(0.2)
+        print("|Choose from an option you like")
+        sleep_after_interaction(0.2)
+        print("___________________________________")
+        sleep_after_interaction(0.2)
+        print("|1 for random number\n"
+              "|2 for the current date and time\n"
+              "|3 to open google\n"
+              "|4 to exit")
+        sleep_after_interaction(0.2)
+        print("-----------------------------------")
+
+        selection_number = int(input())
+
+        if selection_number == 1:
+            print("minimum Number: ")
+            min_num = int(input())
+            print(min_num)
+            print("maximum Number: ")
+            max_num = int(input())
+            print(max_num)
+            ran_num = randint(min_num, max_num)
+            print("-------------------------------------")
+            print("Your Number is: " + str(ran_num))
+            print("-------------------------------------")
+            sleep_after_interaction(0.2)
+
+        if selection_number == 2:
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print("Currently: ", dt_string)
+            sleep_after_interaction(0.2)
+
+        if selection_number == 3:
+            webbrowser.open("https://google.com")
+            print("Link opened")
+
+        if selection_number == 4:
+            print("Bye, have a great day!")
+            break
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import Menu
+import MainMenuMethods
 
 if __name__ == '__main__':
-    Menu
+    print("-------------------------------------------------------")
+    MainMenuMethods.sleep_after_interaction(0.2)
+    print("Welcome! This is my first terminal-based Python Project")
+    MainMenuMethods.sleep_after_interaction(0.2)
+    print("-------------------------------------------------------")
+    MainMenuMethods.sleep_after_interaction(1)
+    MainMenuMethods.start_selected_progress()
